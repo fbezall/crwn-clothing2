3dffe909c78033353d64e74952f66780bb91df30
fbezall <fbezall@gmail.com>
2021-03-16 10:31:33 -1200

    implemented firbase utils, included ability to store authenticated users into ur firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Homepage from '../src/pages/homepage/homepage.component';
 import Shoppage from './pages/shop/shop.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 
@@ -23,10 +23,22 @@ import {auth} from './firebase/firebase.utils';
 
 
   componentDidMount() {
-    this.nsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-
-      console.log(user);
+    this.subscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState ({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({currentUser: userAuth});
+      }
+     
     });
   }
 
